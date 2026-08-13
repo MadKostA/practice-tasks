4b68a9dbe40b7ed6ae30806820e3a9dd79f18bd0
k.madeev <kostyafutbolist@mail.ru>
2026-08-13 22:39:12 +0300

    build: создать модуль для spring-boot задач

---
+++ spring-practice-tasks/src/main/java/org/example/spring_practice_tasks/SpringPracticeTasksApplication.java
@@ -0,0 +1,13 @@
+package org.example.spring_practice_tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPracticeTasksApplication.class, args);
+    }
+
+}
---
+++ spring-practice-tasks/src/test/java/org/example/spring_practice_tasks/SpringPracticeTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring_practice_tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeTasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
