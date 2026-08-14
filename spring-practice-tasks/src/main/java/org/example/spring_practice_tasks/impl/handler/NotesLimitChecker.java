9abcb7e7b0105c4f527ca8a7098478d40685cf4a
k.madeev <kostyafutbolist@mail.ru>
2026-08-15 02:27:43 +0300

    feat: Настраиваемый лимит заметок для dev/prod

---
+++ spring-practice-tasks/src/main/java/org/example/spring_practice_tasks/impl/handler/NotesLimitChecker.java
@@ -0,0 +1,27 @@
+package org.example.spring_practice_tasks.impl.handler;
+
+import org.example.spring_practice_tasks.api.exceptions.NotesCountLimitException;
+import org.example.spring_practice_tasks.impl.config.ProfileSettingsConfig;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotesLimitChecker {
+
+    private final ProfileSettingsConfig config;
+    private final Environment environment;
+
+    public NotesLimitChecker(ProfileSettingsConfig config, Environment environment) {
+        this.config = config;
+        this.environment = environment;
+    }
+
+    public void checkNotesLimit(long totalNotesCount) {
+        if (environment.acceptsProfiles(Profiles.of("dev"))
+                && config.getCountLimit() <= totalNotesCount) {
+
+            throw new NotesCountLimitException(config.getCountLimit());
+        }
+    }
+}
