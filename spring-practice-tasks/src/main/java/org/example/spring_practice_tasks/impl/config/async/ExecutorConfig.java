e4466ec5ae0d0431d0ecbeb446ea5499f73e01fc
k.madeev <kostyafutbolist@mail.ru>
2026-09-06 18:51:48 +0300

    feat: Асинхронное изменение статуса документа (4-5 задания)

--- spring-practice-tasks/src/main/java/org/example/spring_practice_tasks/impl/config/async/ExecutorConfig.java
+++ spring-practice-tasks/src/main/java/org/example/spring_practice_tasks/impl/config/async/ExecutorConfig.java
@@ -12,11 +12,24 @@ import java.util.concurrent.ThreadPoolExecutor;
 @EnableAsync
 public class ExecutorConfig {
 
+    @Bean(name = "documentBatchProcessingExecutor")
+    public Executor documentBatchProcessingExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("document-batch-pool-");
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(30);
+        executor.initialize();
+        return executor;
+    }
+
     @Bean(name = "documentEventLogExecutor")
     public Executor documentEventLogExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(2);
-        executor.setMaxPoolSize(5);
+        executor.setMaxPoolSize(2);
         executor.setQueueCapacity(100);
         executor.setThreadNamePrefix("document-event-log-");
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardPolicy());
--- spring-practice-tasks/src/main/java/org/example/spring_practice_tasks/impl/repo/DocumentRepository.java
+++ spring-practice-tasks/src/main/java/org/example/spring_practice_tasks/impl/repo/DocumentRepository.java
@@ -4,9 +4,12 @@ import org.example.spring_practice_tasks.impl.entity.DocumentEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface DocumentRepository extends MongoRepository<DocumentEntity, UUID> {
+
+    List<DocumentEntity> findAllByStatus(String status);
 
 }
